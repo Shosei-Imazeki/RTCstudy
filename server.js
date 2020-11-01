4886b88295c75193e1f8c3a570f84f00af559d37
Shosei-Imazeki <mon.sho.de@gmail.com>
2020-11-01 20:00:11 +0900

    erver.listenにポート番号を渡して、 サーバーを起動させる
    app.setを用いて、ejsをviewのテンプレートエンジン
    としてセット

--- server.js
+++ server.js
@@ -3,3 +3,6 @@ const app = express();
 const server = require("http").server(app);
 const io = require("socket.io")(server);
 
+server.listen(process.env.PORT || 3030);
+
+app.set("view engine", "ejs");
