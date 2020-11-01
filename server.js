a2653fdaa79ef8729748481ea71295d6ea91f675
Shosei-Imazeki <mon.sho.de@gmail.com>
2020-11-01 20:03:54 +0900

    やjsなどの静的ファイルに対するパスを通す →パスを通すために、app.use()を使用 →app.use関数内に、expressのstatic関数を記述 →static関数の引数に、ディレクトリパスを指定 　今回は、publicディレクトリを使用

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { execPath } = require("process");
 const app = express();
 const server = require("http").server(app);
 const io = require("socket.io")(server);
@@ -6,3 +7,4 @@ const io = require("socket.io")(server);
 server.listen(process.env.PORT || 3030);
 
 app.set("view engine", "ejs");
+app.use(express.static("public"));
